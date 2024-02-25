9f95bcf9fb2a421b89d5c058d743518b5bc1a04d
ariyha <96973647+ariyha@users.noreply.github.com>
2024-02-25 23:08:46 +0530

    Time: 147 ms (98.44%), Space: 19.7 MB (88.37%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -15,9 +15,3 @@ class MyHashMap:
     def remove(self, key: int) -> None:
         self.mydict[key]=-1
 
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
