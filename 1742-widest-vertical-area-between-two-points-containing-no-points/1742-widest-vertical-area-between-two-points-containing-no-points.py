702a86a9111c3b32ac62e5a780833a3bca181817
ariyha <96973647+ariyha@users.noreply.github.com>
2024-03-01 23:26:34 +0530

    Time: 692 ms (60.25%), Space: 58.5 MB (87.43%) - LeetHub

--- 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.py
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.py
@@ -3,9 +3,8 @@ class Solution:
         l.sort(key=lambda x:x[0])
         max = l[0][0]-l[1][0]
         for i in range(1,len(l)):
-            k=l[i][0]-l[i-1][0]
-            if k>max:
-                max=k
+            if l[i][0]-l[i-1][0]>max:
+                max=l[i][0]-l[i-1][0]
         return max
 
 
