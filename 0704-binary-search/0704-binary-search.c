e19682a349256597f1ac7506c80b4d3502663132
ariyha <96973647+ariyha@users.noreply.github.com>
2024-03-10 12:16:48 +0530

    Time: 22 ms (92.98%), Space: 6.8 MB (62.2%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,22 @@
+int search(int* nums, int n, int target) {
+    int high = n-1;
+    int low=0;
+    int mid;
+
+    while(low<=high){
+        mid = high-low/2;
+        if (nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]>target){
+            high=mid-1;
+            continue;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return -1;
+
+    
+}
