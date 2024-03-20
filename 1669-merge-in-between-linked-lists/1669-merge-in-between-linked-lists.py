aa37d0b8b3ee989e68a95ef33bde42ae94588071
ariyha <96973647+ariyha@users.noreply.github.com>
2024-03-20 07:54:42 +0530

    Time: 184 ms (85.82%), Space: 21 MB (87.43%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -10,8 +10,6 @@ class Solution:
                 k2=h1.next.next
             c1=c1+1
             h1=h1.next
-            if(h2.next):
-                h2=h2.next   
         while(h2.next):
             h2=h2.next     
         k1.next = list2
