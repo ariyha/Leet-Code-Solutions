96e9096df06916250d107e039510512c3411fd23
ariyha <96973647+ariyha@users.noreply.github.com>
2024-03-23 12:08:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow=fast=head
+        while(fast.next and fast.next.next):
+            slow=slow.next
+            fast=fast.next.next
+        
+        prev=None
+        curr = slow.next
+        
+        while(curr):
+            nex = curr.next
+            curr.next=prev
+            prev=curr
+            curr=nex
+
+        slow.next=None
+
+        a=head
+        b=prev
+        while(a and b):
+            anex=a.next
+            bnex=b.next
+
+            a.next=b
+            b.next=anex
