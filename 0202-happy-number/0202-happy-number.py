8539c8a442f1ff838bf135c26b64647adb16d040
ariyha <96973647+ariyha@users.noreply.github.com>
2024-07-17 09:45:01 +0530

    Time: 33 ms (80.41%), Space: 16.5 MB (21.63%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        known = []
+        known = set()
         if n==1 or n==7:
             return True
 
         while(n not in known and n!=1 and n!=7):
-            known.append(n)
+            known.add(n)
             n = str(n)
             n = sum([int(i)**2 for i in n])
             
