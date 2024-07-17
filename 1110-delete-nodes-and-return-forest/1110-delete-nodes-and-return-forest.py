8aaeb79ccd28dc9c864c01735a648ae6a1868bce
ariyha <96973647+ariyha@users.noreply.github.com>
2024-07-17 09:30:26 +0530

    Time: 69 ms (8.52%), Space: 17 MB (24.3%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -7,7 +7,6 @@ class TreeNode:
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
         root.root = True
-        root.lr = 100
         tovisit = [root]
         nodes = []
 
@@ -15,42 +14,31 @@ class Solution:
             x = tovisit.pop()
 
             if x.val in to_delete:
-                # if x.lr ==1:
-                #     x.parent.right = None
-                # elif x.lr==2:
-                #     x.parent.left = None
-                # elif x.lr ==100:
-                #     pass
-        
                 if x.right:
                     x.right.root = True
-                    # x.right.parent = x
-                    # x.right.lr = 1
                     tovisit.append(x.right)
                 
                 if x.left:
                     x.left.root = True
-                    # x.left.parent = x
-                    # x.left.lr = 2
                     tovisit.append(x.left)
-            else:
-                if x.root:
-                    nodes.append(x)
                 
-                if x.right:
-                    x.right.root = False
-                    # x.right.parent = x
-                    tovisit.append(x.right)
-                    if x.right.val in to_delete:
-                        x.right = None
+                continue
 
-                
-                if x.left:
-                    # x.left.parent = x
-                    x.left.root = False
-                    tovisit.append(x.left)
-                    if x.left.val in to_delete:
-                        x.left = None
+            if x.root:
+                nodes.append(x)
+            
+            if x.right:
+                x.right.root = False
+                tovisit.append(x.right)
+                if x.right.val in to_delete:
+                    x.right = None
+
+            
+            if x.left:
+                x.left.root = False
+                tovisit.append(x.left)
+                if x.left.val in to_delete:
+                    x.left = None
         
     
         return nodes
