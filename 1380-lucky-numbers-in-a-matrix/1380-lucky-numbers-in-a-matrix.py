2dec6e4fd578add60b019de453b6752f2e5c6787
ariyha <96973647+ariyha@users.noreply.github.com>
2024-07-19 09:05:23 +0530

    Time: 124 ms (18.08%), Space: 16.9 MB (15.89%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        mins =[]
+        for i in range(len(matrix)):
+            mins.append(min(matrix[i]))
+        
+        maxs = []
+        
+        for i in range(len(matrix[0])):
+            maxi = float(-inf)
+            for j in range(len(matrix)):
+                maxi = max(matrix[j][i],maxi)
+            
+            maxs.append(maxi)
+
+        out =[]
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                    if matrix[i][j]==mins[i] and matrix[i][j]==maxs[j]:
+                        out.append(matrix[i][j])
+
+        return out        
