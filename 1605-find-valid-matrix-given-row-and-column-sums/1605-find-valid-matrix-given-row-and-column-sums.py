ad2dbb2744d37117a90ad5d8aa0229cedb946df7
ariyha <96973647+ariyha@users.noreply.github.com>
2024-07-20 23:09:33 +0530

    Time: 367 ms (91.34%), Space: 21.8 MB (53.07%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -1,27 +1,46 @@
-class Solution:
-    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+# class Solution:
+#     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
 
-        n = len(rowSum)
-        m = len(colSum)
-        i,j = 0,0
+#         n = len(rowSum)
+#         m = len(colSum)
+#         i,j = 0,0
 
-        arr = []
-        for i in range(m):
-            arr.append([0]*n)
+#         arr = []
+#         for i in range(m):
+#             arr.append([0]*n)
 
-        while(i<n and j<m):
-            x = min(rowSum[i],colSum[j])
-            arr[i][j]=x
+#         while(i<n and j<m):
+#             x = min(rowSum[i],colSum[j])
+#             arr[i][j]=x
 
-            rowSum[i]-=x
-            colSum[j]-=x
+#             rowSum[i]-=x
+#             colSum[j]-=x
 
-            if rowSum[i]==0:
-                i+=1
+#             if rowSum[i]==0:
+#                 i+=1
             
-            if colSum[j]==0:
-                j+=1
+#             if colSum[j]==0:
+#                 j+=1
 
-        return arr
+#         return arr
 
+class Solution(object):
+    def restoreMatrix(self, rowSum, colSum):
+        numRows = len(rowSum)
+        numCols = len(colSum)
+        result = [[0] * numCols for _ in range(numRows)]
+
+        i, j = 0, 0
+
+        while i < numRows and j < numCols:
+            val = min(rowSum[i], colSum[j])
+            result[i][j] = val
+            rowSum[i] -= val
+            colSum[j] -= val
+
+            if rowSum[i] == 0:
+                i += 1
+            if colSum[j] == 0:
+                j += 1
         
+        return result
