33ae2d6e2c6db793fbee5d67bf90096d24ae04d1
Nithish Ariyha K <96973647+ariyha@users.noreply.github.com>
2025-01-09 14:36:16 +0530

    Time: 0 ms (100%), Space: 42.9 MB (53.95%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -3,12 +3,16 @@ class Solution {
         int count=0;
         int l1 = pref.length();
         for(String word: words){
-            if(word.length()<l1){
-                continue;
-            }
+            // if(word.length()<l1){
+            //     continue;
+            // }
 
-            if(word.substring(0,l1).equals(pref)){
-                count++;
+            // if(word.substring(0,l1).equals(pref)){
+            //     count++;
+            // }
+
+            if (word.startsWith(pref)){
+                count+=1;
             }
         }
 
